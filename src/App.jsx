8507bdba34c7d910b13cede6d53f63579384f8c3
MarijanaHornjak <marijanahornjak95@gmail.com>
2023-09-12 17:38:06 +0200

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import "./fetchProjects";
+import About from "./pages/About/About";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <About /> },
+  { path: "/about", element: <h1>about</h1> },
+  { path: "/contact", element: <h1>contact</h1> },
+  { path: "/plants", element: <h1>plants</h1> },
+  { path: "/care", element: <h1>care</h1> },
+]);
 
 function App() {
   return (
     <>
-      <h2>Hello from App</h2>
+      <About />
     </>
   );
 }
